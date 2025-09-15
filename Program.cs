ba9aee382694c9aa7c60e0a454a5027306a33227
Lapma <lapostyanm@kkszki.hu>
2025-09-15 11:04:56 +0200

    MySQL connection is sucessfull.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlClient;
+using System;
+using MySql.Data.MySqlClient;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Adja meg az adatbázis nevét: ");
+        string databaseName = Console.ReadLine();
+
+        Console.Write("Adja meg a felhasználónevet: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Adja meg a jelszót: ");
+        string password = Console.ReadLine();
+        string connectionString = $"Server=localhost;Database={databaseName};User Id={username};Password={password};";
+
+        try
+        {
+            
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Sikeres csatlakozás");
+            }
+        }
+        catch (Exception ex)
+        {
+            
+            Console.WriteLine($"Hiba a csatlakozás során: {ex.Message}");
+        }
+
+    }
+}
